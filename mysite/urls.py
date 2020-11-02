6ff2c0478d6bb63ae71d2095a40e92339188c1ea
Roberto Betini Junior <55061623+robertobetini@users.noreply.github.com>
2020-11-01 22:04:01 -0300

    Adding the files

---
+++ mysite/urls.py
@@ -0,0 +1,42 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import os
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls import url
+from django.views.static import serve
+from django.views.generic import TemplateView
+from ads.views import AdListView
+
+# Up two folders to serve "site" content
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_ROOT = os.path.join(BASE_DIR, 'site')
+
+urlpatterns = [
+    path('', AdListView.as_view(template_name='ads/ad_list.html')),
+    #path('', TemplateView.as_view(template_name='home/main.html')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('hello/', include('hello.urls')),
+    path('autos/', include('autos.urls')),
+    path('cats/', include('cats.urls')),
+    path('ads/', include('ads.urls')),
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    url(r'^site/(?P<path>.*)$', serve,
+        {'document_root': SITE_ROOT, 'show_indexes': True},
+        name='site_path'
+    ),
+]
